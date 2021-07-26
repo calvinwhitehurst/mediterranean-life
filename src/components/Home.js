b596bb04ddd86378f80f97cf96fbe2c1d678610c
Calvin Whitehurst <calvin@calvinwhitehurst.com>
2021-07-25 17:03:49 -0700

    added recipes page and connection to sanity

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React from "react";
+import image from "../hero-card.jpg";
 
 export default function Home() {
-    return <h1>Home</h1>
+  return (
+    <main>
+      <img src={image} alt="mediterrnean food" className="absolute object-cover w-full h-full"/>
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg: leading-snug home-name">This is the home page</h1>
+      </section>
+    </main>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
     return (
@@ -28,6 +29,9 @@ export default function NavBar() {
                         About
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon url="https://www.instagram.com/calvinwhitehurst/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }}/>
+                </div>
             </div>
         </header>
     )
