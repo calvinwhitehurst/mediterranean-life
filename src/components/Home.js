cb010ca96558ffe2a9755b1fd608cc8da7dce8e8
Calvin Whitehurst <calvin@calvinwhitehurst.com>
2021-07-26 20:59:20 -0700

    added about page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,15 @@ import image from "../hero-card.jpg";
 export default function Home() {
   return (
     <main>
-      <img src={image} alt="mediterrnean food" className="absolute object-cover w-full h-full"/>
+      <img
+        src={image}
+        alt="mediterrnean food"
+        className="absolute object-cover w-full h-full"
+      />
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg: leading-snug home-name">This is the home page</h1>
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg: leading-snug home-name">
+          This is the home page
+        </h1>
       </section>
     </main>
   );
